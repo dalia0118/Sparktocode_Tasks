e4276283f5a068d9dfdef1ae65afa018cb26cfae
aria2 <22f22728@mec.edu.om>
2026-07-01 11:44:07 +0400

    add easy: task 2

--- Sparktocode C# Fundamentals/Program.cs
+++ Sparktocode C# Fundamentals/Program.cs
@@ -13,6 +13,19 @@ namespace Csharpfundamentals
             double height = 1.56;
             bool isStudent = false;
             Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height + ", Student: " + isStudent);
+
+            // Easy: Task 2
+
+            Console.Write("Enter the length of the rectangle: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double Area = length * width;
+            double Perimeter = 2 * (length + width);
+            Console.WriteLine("Area of the rectangle is: " + Area + ", and the perimeter is: " + Perimeter +".");
+
+
         }
     }
 }
