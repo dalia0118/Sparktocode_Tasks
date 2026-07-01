a39852f153f39aa4a1badbbd161b567bb6fb72c5
aria2 <22f22728@mec.edu.om>
2026-07-01 11:34:26 +0400

    Add easy: task 1

--- Sparktocode C# Fundamentals/Program.cs
+++ Sparktocode C# Fundamentals/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection;
+namespace Csharpfundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Easy: Task 1
+
+            string name = "Dalia";
+            int age = 21;
+            double height = 1.56;
+            bool isStudent = false;
+            Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height + ", Student: " + isStudent);
+        }
+    }
+}
