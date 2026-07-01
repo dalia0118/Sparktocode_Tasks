e125f37666c1f19772ef345d3739a87aace3bc1f
aria2 <22f22728@mec.edu.om>
2026-07-01 11:51:57 +0400

    Add easy: task 3

--- Sparktocode C# Fundamentals/Program.cs
+++ Sparktocode C# Fundamentals/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Csharpfundamentals
 {
     class Program
@@ -13,6 +14,7 @@ namespace Csharpfundamentals
             double height = 1.56;
             bool isStudent = false;
             Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height + ", Student: " + isStudent);
+            Console.Write("\n");
 
             // Easy: Task 2
 
@@ -24,8 +26,24 @@ namespace Csharpfundamentals
             double Area = length * width;
             double Perimeter = 2 * (length + width);
             Console.WriteLine("Area of the rectangle is: " + Area + ", and the perimeter is: " + Perimeter +".");
+            Console.Write("\n");
+
+            // Easy: Task 3
+
+            Console.Write("Enter a whole number: ");
+            int wholenumber = Convert.ToInt32(Console.ReadLine());
+            int remainder = wholenumber % 2;
+            if (remainder == 0)
+            {
+                Console.WriteLine(wholenumber + " is even");
+            }
+            else
+            {
+                Console.WriteLine(wholenumber + " is odd");
+            }
+            Console.Write("\n");
 
 
         }
-    }
+}
 }
