7bc92ec24bb5432d5470d84aa01dca71daee9105
aria2 <22f22728@mec.edu.om>
2026-07-05 13:24:10 +0400

    Add solution for Task 2 - Sum of Numbers 1 to N

--- C# Fundamentals - Part 2/Program.cs
+++ C# Fundamentals - Part 2/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             // Easy: Task 1 - Countdown Timer
-            Console.WriteLine("Enter a starting number: ");
+
+            Console.WriteLine("Enter a starting number for a countdown: ");
             int start = Convert.ToInt32(Console.ReadLine());
 
             for (int i = start; i >= 1; i--)
@@ -14,6 +15,21 @@
             }
 
             Console.WriteLine("Liftoff!");
+            Console.Write("\n");
+
+
+            // Easy: Task 2 - Sum of Numbers 1 to N
+
+            Console.WriteLine("Enter a positive whole number N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("Sum = " + sum);
             Console.Write("\n");
 
 
