1afef8bd99cca163476336701a332fe0e9ea088e
aria2 <22f22728@mec.edu.om>
2026-07-05 14:44:37 +0400

    Implement solution for Task 2 - Power & Root Explorer

--- C# Fundamentals - Part 3/Program.cs
+++ C# Fundamentals - Part 3/Program.cs
@@ -12,9 +12,23 @@
             double num2 = Convert.ToDouble(Console.ReadLine());
             double difference = Math.Abs(num1 - num2);
             Console.WriteLine("The absolute difference is " + difference);
+            Console.Write("\n");
 
 
-            
+            // Easy: Task 2 - Power & Root Explorer
+
+            Console.WriteLine("Enter a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            double square = Math.Pow(number, 2);
+            double squareRoot = Math.Sqrt(number);
+
+            Console.WriteLine("Square = " + square);
+            Console.WriteLine("Square Root = " + squareRoot);
+            Console.Write("\n");
+
+
+
         }
     }
 }
