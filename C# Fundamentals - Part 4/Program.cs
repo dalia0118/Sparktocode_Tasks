41ebca4de929e95fbacaa936e1a6e981d72c7c8e
aria2 <22f22728@mec.edu.om>
2026-07-07 12:37:40 +0400

    Implement solution for Task 1 - Personalized Welcome Function

---
+++ C# Fundamentals - Part 4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace C__Fundamentals___Part_4
+{
+    internal class Program
+    {
+        // Easy: Task 1 - Personalized Welcome Function
+        static void PrintWelcome(string uname)
+        {
+            Console.WriteLine("Welcome " + uname + "!! Have a good day.");
+        }
+
+        static void Main(string[] args)
+        {
+            {
+                // Easy: Task 1 - Personalized Welcome Function
+                Console.Write("Enter your name: ");
+                string userName = Console.ReadLine();
+                PrintWelcome(userName);
+                Console.Write("\n");
+
+
+
+            }
+        }
+    }
+}
